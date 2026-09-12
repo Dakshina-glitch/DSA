3db7018b6a6ba1457b0a307d05490cbddda6cb5e
Dakshina-glitch <itami@Himanshus-MacBook-Air.local>
2026-09-13 00:29:27 +0530

    recursion practice

--- Recursion/src/main/java/FirstNonRepeatingCharacter.java
+++ Recursion/src/main/java/FirstNonRepeatingCharacter.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
+import java.util.Map;
 
 public class FirstNonRepeatingCharacter {
+    public static void main(String[] args) {
+        String s = "lleetcode";
+        System.out.println(firstUniqueChar(s));
+    }
+
+    static public int firstUniqueChar(String s) {
+        Map<Character, Integer> freq = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            freq.put(c, freq.getOrDefault(c, 0) + 1);
+        }
+
+        return solve(s, 0, freq);
+    }
+
+    static private int solve(String s, int index, Map<Character, Integer> freq) {
+        if (index >= s.length()) {
+            return -1;
+        }
+
+        if (isFirstUniqueCharacter(s.charAt(index), freq)) {
+            return index;
+        }
+
+        return solve(s, index + 1, freq);
+    }
+
+    static private boolean isFirstUniqueCharacter(char c, Map<Character, Integer> freq) {
+        return freq.get(c) == 1;
+    }
 }
